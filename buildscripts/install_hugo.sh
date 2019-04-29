46d607b195732bf6b56babf8f026ba481219efb6
Harrtron <harleyjthorne@gmail.com>
2019-04-29 15:00:00 +0100

    Switch to GO installer for Hugo

--- buildscripts/install_hugo.sh
+++ buildscripts/install_hugo.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
-url=$(curl -s "https://api.github.com/repositories/11180687/releases/latest" | grep -o 'https://.*hugo_extended.*_macOS-64bit.tar.gz')
-echo '✅ Found latest version'
-
-curl -s $url -L -o hugo_latest.tar.gz
-echo '✅ Download complete: ' $url
-
-tar -zxf hugo_latest.tar.gz -C /usr/local/bin
-rm /usr/local/bin/README.md
-rm /usr/local/bin/LICENSE
-echo '✅ Extracted to /usr/local/bin'
-
-rm hugo_latest.tar.gz
-echo '✅ Removed downloaded artifacts'
-
-echo '👉 Current Version' $(hugo version)
+mkdir $HOME/src
+cd $HOME/src
+git clone https://github.com/gohugoio/hugo.git
+cd hugo
+go install --tags extended
