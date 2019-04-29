09333fd84cbfbe4bd52c065e76315bf9afddbc48
Harrtron <harleyjthorne@gmail.com>
2019-04-29 13:15:56 +0100

    Fix access keys for s3 upload?

--- buildscripts/upload_s3.sh
+++ buildscripts/upload_s3.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "Uploading to s3"
-aws s3 sync public/ s3://harleythorne.com --delete --aws-access-key $1 --aws-secret-key $2
+export AWS_ACCESS_KEY_ID=$1
+export AWS_SECRET_ACCESS_KEY=$2
+aws s3 sync public/ s3://harleythorne.com --delete
